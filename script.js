9576bd49b1ef2853be3669ff02d9af55ebebe720
shihab-wrizz <139270286+shihab-wrizz@users.noreply.github.com>
2023-07-13 01:42:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const main_video = document.querySelector('.main-video video');
+const main_video_title = document.querySelector('.main-video .title');
+const video_playlist = document.querySelector('.video-playlist .videos');
+
+let data = [
+    {
+        'id': 'a1',
+        'title': 'Leak Subha Video #1',
+        'name': 'subha-1.mp4',
+        'duration': 'NA',
+    },
+    {
+        'id': 'a2',
+        'title': 'Leak Subha Video #2',
+        'name': 'subha-2.mp4',
+        'duration': 'NA',
+    },
+    {
+        'id': 'a3',
+        'title': 'Leak Subha Video #3',
+        'name': 'subha-3.mp4',
+        'duration': 'NA',
+    },
+];
+
+data.forEach((video, i) => {
+    let video_element = `
+                <div class="video" data-id="${video.id}">
+                    <img src="images/play.svg" alt="">
+                    <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
+                    <h3 class="title">${video.title}</h3>
+                    <p class="time">${video.duration}</p>
+                </div>
+    `;
+    video_playlist.innerHTML += video_element;
+})
+
+let videos = document.querySelectorAll('.video');
+videos[0].classList.add('active');
+videos[0].querySelector('img').src = 'images/pause.svg';
+
+videos.forEach(selected_video => {
+    selected_video.onclick = () => {
+
+        for (all_videos of videos) {
+            all_videos.classList.remove('active');
+            all_videos.querySelector('img').src = 'images/play.svg';
+
+        }
+
+        selected_video.classList.add('active');
+        selected_video.querySelector('img').src = 'images/pause.svg';
+
+        let match_video = data.find(video => video.id == selected_video.dataset.id);
+        main_video.src = 'videos/' + match_video.name;
+        main_video_title.innerHTML = match_video.title;
+    }
+});
